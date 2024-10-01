6ce8fecd16947b6144e9c1b9b870998259888135
Emmanuel <theemanuel211@gmail.com>
2024-10-01 12:03:11 -0600

    I have used more methods and annotations in the MockMVC section

--- src/test/java/com/emmanuel/swagger/api/controller/ProductControllerTest.java
+++ src/test/java/com/emmanuel/swagger/api/controller/ProductControllerTest.java
@@ -9,9 +9,11 @@ import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.atMostOnce;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -19,6 +21,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -40,49 +45,84 @@ public class ProductControllerTest {
 	
 	@BeforeEach
 	void createObjectMapper() {
-		this.objectMapper = new ObjectMapper(); 
-		
-		
+		this.objectMapper = new ObjectMapper(); 	
 	}
 	
-	@Test
-	void testGetProduct() throws Exception{
-		//Given
-		List<ProductDtoRequest> products = List.of(new ProductDtoRequest(1,"Laptop", 200), new ProductDtoRequest(2,"Ipad", 300));
-		when(IProductService.getAllProducts()).thenReturn(products);
+	@Nested
+	@DisplayName(value="This is the Get Family Nested Class")
+	class getTestCases{
+	
+		@Test
+		@Tag(value="Get Family")
+		@DisplayName(value="Get all the products")
+		void testGetAllProducts() throws Exception{
+			//Given
+			List<ProductDtoRequest> products = List.of(new ProductDtoRequest(1,"Laptop", 200), new ProductDtoRequest(2,"Ipad", 300));
+			when(IProductService.getAllProducts()).thenReturn(products);
+			
+			//When
+			mockProductController.perform(get("/products/getAll"))
+					.andExpect(status().isOk())
+					.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			//Then
+					.andExpect(jsonPath("$[0].name").value("Laptop"))
+					.andExpect(jsonPath("$[0].price").value(200))
+					.andExpect(jsonPath("$[0].id").exists())
+					.andExpect(jsonPath("$[0].proveedor").doesNotExist())
+					.andExpect(jsonPath("$.length()").value(2))
+					.andExpect(jsonPath("$").isArray());
+			
+			verify(IProductService, atMostOnce()).getAllProducts();
+			verify(IProductService, atMost(1)).getAllProducts();
+			verify(IProductService, atLeastOnce()).getAllProducts();
+			verify(IProductService, atLeast(1)).getAllProducts();
+			verify(IProductService, never()).getOneProduct(anyInt());
+		}
 		
-		mockProductController.perform(get("/products/getAll"))
+		@Test
+		@Tag(value="Get Family")
+		@DisplayName(value="Get One Product")
+		void testGetOneProduct() throws Exception{
+			
+			//Given
+			when(IProductService.getOneProduct(1)).thenReturn(new ProductDtoRequest(1,"Laptop", 200));
+			
+			//When
+			mockProductController.perform(get("/products/getOne/{idProduct}", 1))
+			//Then
 				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name").value("Laptop"))
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$[0].name").value("Laptop"))
-				.andExpect(jsonPath("$[0].price").value(200));
-		
-		verify(IProductService, atMostOnce()).getAllProducts();
-		verify(IProductService, atMost(1)).getAllProducts();
-		verify(IProductService, atLeastOnce()).getAllProducts();
-		verify(IProductService, atLeast(1)).getAllProducts();
-		verify(IProductService, never()).getOneProduct(anyInt());
+				.andExpect(jsonPath("$.expiration").doesNotExist())
+				.andExpect(jsonPath("$.price").exists());
+			
+			
+			verify(IProductService, atLeastOnce()).getOneProduct(anyInt());
+			verify(IProductService, atMostOnce()).getOneProduct(1);
+			verify(IProductService, never()).getAllProducts();
+		}
 	}
 	
 	@Test
-	void testOneProduct() throws Exception{
+	void testCreateOneProduct() throws Exception {
 		
-		when(IProductService.getOneProduct(1)).thenReturn(new ProductDtoRequest(1,"Laptop", 200));
+		//Given
+		ProductDtoRequest product = new ProductDtoRequest(100, "Cepillo de Dientes", 11);
+		String productString = this.objectMapper.writeValueAsString(product);
+		when(IProductService.createOneProduct(any(ProductDtoRequest.class))).thenReturn(product);
 		
-		mockProductController.perform(get("/products/getOne/1")).andExpect(status().isOk())
-			.andExpect(jsonPath("$.name").value("Laptop"));
-	}
-	
-	
-	@Test
-	void createOneProduct() throws Exception {
+		//When
+		mockProductController.perform(post("/products/create")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(productString))
+		//Then
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.id").isNumber())
+				.andExpect(jsonPath("$.name").isString())
+				.andExpect(jsonPath("$.lastModifiedBy").doesNotExist())
+				.andExpect(jsonPath("$.name").value("Cepillo de Dientes"));
 		
-		ProductDtoRequest product = new ProductDtoRequest(100,"Cepillo de Dientes", 11);
-		
-		String roductString = objectMapper.writeValueAsString(product);
-		
-		mockProductController.perform(post("/products/create").contentType(MediaType.APPLICATION_JSON)
-				.content(roductString))
-		.andExpect(status().isCreated());
+		verify(IProductService, times(1)).createOneProduct(any(ProductDtoRequest.class));
+		verify(IProductService, never()).deleteProduct(anyInt());
 	}
 }
