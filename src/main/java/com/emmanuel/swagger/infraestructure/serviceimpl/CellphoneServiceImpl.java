3d67afd6e4ce4d2e2dea2261961957d9dc70e686
Emmanuel <theemanuel211@gmail.com>
2024-10-01 22:57:59 -0600

    I have added Fault Tolerance by Spring Cloud CircuitBreaker & Resilience4j using de configuracion file.

--- src/main/java/com/emmanuel/swagger/infraestructure/serviceimpl/CellphoneServiceImpl.java
+++ src/main/java/com/emmanuel/swagger/infraestructure/serviceimpl/CellphoneServiceImpl.java
@@ -1,8 +1,11 @@
-package com.emmanuel.swagger.infraestructure.service;
+package com.emmanuel.swagger.infraestructure.serviceimpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.emmanuel.swagger.infraestructure.feign.clients.ICelularClient;
 import com.emmanuel.swagger.infraestructure.feign.dtoresponse.Celular;
+import com.emmanuel.swagger.infraestructure.service.ICellphoneService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @Service
 public class CellphoneServiceImpl implements ICellphoneService{
@@ -15,10 +18,22 @@ public class CellphoneServiceImpl implements ICellphoneService{
     }
 	
 	@Override
+	@CircuitBreaker(name="cellphones", fallbackMethod="fallBackCelular")
 	public Celular getCellPhone() {
 		Celular my_cellphone = iCelularClient.getCelularFromFeignClient();
 		System.out.println(my_cellphone);
 		return my_cellphone;
 	}
 
+	public Celular fallBackCelular(Exception e) {
+		
+		Celular responseCelular = new Celular();
+		responseCelular.setModelo("La petición ha fallado");
+		responseCelular.setMarca("Intente más tarde");
+		responseCelular.setPrice(0);
+		
+		return responseCelular;
+		
+	}
+	
 }
